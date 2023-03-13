b5539c3a4b73c3aa3feb2f8ccd3850ac140de495
Rahul Bagai <rahul.bagai2006@gmail.com>
2023-03-13 23:24:04 +0000

    slack integration completed

--- simple-bot-test/src/main/java/com/aws/bot/api/APILambda.java
+++ simple-bot-test/src/main/java/com/aws/bot/api/APILambda.java
@@ -7,51 +7,52 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.aws.bot.dao.Contact;
 import com.aws.bot.services.DynamoDBService;
 import com.google.gson.Gson;
+import com.slack.api.Slack;
 
 public class APILambda implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
-
     private final DynamoDBService dynamoDBService = new DynamoDBService();
     private final SlackBot slackBot = new SlackBot();
+    private final Slack slack = Slack.getInstance();
     private final Gson gson = new Gson();
     private static final Logger LOGGER = LoggerFactory.getLogger(APILambda.class);
 
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
-        LOGGER.debug("request: " + gson.toJson(request));
+        try {
+            LOGGER.debug("request: " + gson.toJson(request));
 
-        String httpMethod = request.getHttpMethod();
-        String path = request.getPath();
-        LOGGER.debug(null, "HTTP Method: {}, Path: {}", httpMethod, path);
+            String httpMethod = request.getHttpMethod();
+            String path = request.getPath();
+            LOGGER.debug(null, "HTTP Method: {}, Path: {}", httpMethod, path);
 
-        if(httpMethod.equals("GET") && path.equals("/contacts")) {
-            return listContacts(request);
-        } 
-        else if(httpMethod.equals("POST") && path.equals("/contacts")) {
-            return createContact(request);
-        } 
-        else if (httpMethod.equals("POST") && path.equals("/slackbotevents")) {
-            return slackBot.handleEvent(request, context);
+            if (httpMethod.equals("GET") && path.equals("/contacts")) {
+                return listContacts(request);
+            } else if (httpMethod.equals("POST") && path.equals("/contacts")) {
+                return createContact(request);
+            } else if (httpMethod.equals("POST") && path.equals("/slackbotevents")) {
+                return slackBot.handleEvent(request, context, slack);
+            }
+        } catch (Exception ex) {
+            LOGGER.error("Exception: {}", ex);
         }
-        else {
-            return new APIGatewayProxyResponseEvent()
-            .withStatusCode(404)
-            .withBody("Not found");
-        }
+
+        return new APIGatewayProxyResponseEvent()
+                .withStatusCode(404)
+                .withBody("Not found");
     }
 
     private APIGatewayProxyResponseEvent listContacts(APIGatewayProxyRequestEvent input) {
         List<Contact> contacts = dynamoDBService.listContacts();
 
         return new APIGatewayProxyResponseEvent()
-        .withStatusCode(200)
-        .withBody(gson.toJson(contacts))
-        .withHeaders(Collections.singletonMap("Content-Type", "application/json"));
+                .withStatusCode(200)
+                .withBody(gson.toJson(contacts))
+                .withHeaders(Collections.singletonMap("Content-Type", "application/json"));
     }
 
     private APIGatewayProxyResponseEvent createContact(APIGatewayProxyRequestEvent input) {
